bc3b5513b9b5e0806d3ba1122e52d44f3be18663
HackRaindrop <jwkirk15@gmail.com>
2025-09-20 14:09:23 -0400

    Removal of optional chaining and leftover declarations

--- src/responses.js
+++ src/responses.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 const css = fs.readFileSync(`${__dirname}/../client/style.css`);
 
-//Respond with status code
+// Respond with status code
 const respond = (request, response, content, type, status = 200) => {
   response.writeHead(status, {
     'Content-Type': type,
@@ -13,18 +13,18 @@ const respond = (request, response, content, type, status = 200) => {
   response.end();
 };
 
-//Respond with JSON
+// Respond with JSON
 const respondJSON = (request, response, status, object) => {
   const content = JSON.stringify(object);
   respond(request, response, content, 'application/json', status);
 };
 
-//Respond with XML
+// Respond with XML
 const respondXML = (request, response, status, object) => {
-  //Create string
+  // Create string
   let content = '<response>';
 
-  //Add message and ID if it exists
+  // Add message and ID if it exists
   content += `<message>${object.message}</message>`;
   if (object.id) {
     content += `<id>${object.id}</id>`;
@@ -34,7 +34,7 @@ const respondXML = (request, response, status, object) => {
   respond(request, response, content, 'text/xml', status);
 };
 
-//Unified response function that detects format automatically
+// Unified response function that detects format automatically
 const respondWithFormat = (request, response, status, responseData) => {
   const acceptHeader = request.headers.accept || 'application/json';
   const useXML = acceptHeader.includes('text/xml');
@@ -46,17 +46,17 @@ const respondWithFormat = (request, response, status, responseData) => {
   }
 };
 
-//Index of page
+// Index of page
 const getIndex = (request, response) => {
   respond(request, response, index, 'text/html');
 };
 
-//CSS file
+// CSS file
 const getCSS = (request, response) => {
   respond(request, response, css, 'text/css');
 };
 
-//Success response
+// Success response
 const success = (request, response) => {
   const responseData = {
     message: 'This is a successful response',
@@ -65,49 +65,46 @@ const success = (request, response) => {
   respondWithFormat(request, response, 200, responseData);
 };
 
-//Bad Request response
+// Bad Request response
 const badRequest = (request, response, params) => {
-  //Default response
+  // Default response
   const responseData = {
     message: 'This request has the required parameters',
   };
 
-  //If parameter is missing or false, send with error code
-  if (params?.query?.valid !== 'true') {
-    const acceptHeader = request.headers.accept || 'application/json';
-    const useXML = acceptHeader.includes('text/xml');
-
+  // If parameter is missing or false, send with error code
+  if (!params || !params.query || params.query.valid !== 'true') {
     responseData.message = 'Missing valid query parameter set to true';
     responseData.id = 'badRequest';
 
     return respondWithFormat(request, response, 400, responseData);
   }
 
-  //If parameter exists, send with success code
+  // If parameter exists, send with success code
   return respondWithFormat(request, response, 200, responseData);
 };
 
-//Unauthorized response
+// Unauthorized response
 const unauthorized = (request, response, params) => {
-  //Default response
+  // Default response
   const responseData = {
     message: 'You have successfully viewed the content.',
   };
 
-  //If parameter is missing or false, send with 401 code
-  if (params?.query?.loggedIn !== 'yes') {
-    //New message and ID
+  // If parameter is missing or false, send with 401 code
+  if (!params || !params.query || params.query.loggedIn !== 'yes') {
+    // New message and ID
     responseData.message = 'Missing loggedIn query parameter set to yes';
     responseData.id = 'unauthorized';
 
     return respondWithFormat(request, response, 401, responseData);
   }
 
-  //If parameter exists, send with success code
+  // If parameter exists, send with success code
   return respondWithFormat(request, response, 200, responseData);
 };
 
-//Forbidden response
+// Forbidden response
 const forbidden = (request, response) => {
   const responseData = {
     message: 'You do not have access to this content.',
@@ -117,7 +114,7 @@ const forbidden = (request, response) => {
   respondWithFormat(request, response, 403, responseData);
 };
 
-//Internal Server Error response
+// Internal Server Error response
 const internal = (request, response) => {
   const responseData = {
     message: 'Internal Server Error. Something went wrong.',
@@ -127,7 +124,7 @@ const internal = (request, response) => {
   respondWithFormat(request, response, 500, responseData);
 };
 
-//Not Implemented response
+// Not Implemented response
 const notImplemented = (request, response) => {
   const responseData = {
     message: 'A get request for this page has not been implemented yet. Check again later for updated content.',
@@ -137,7 +134,7 @@ const notImplemented = (request, response) => {
   respondWithFormat(request, response, 501, responseData);
 };
 
-//Not Found response
+// Not Found response
 const anythingElse = (request, response) => {
   const responseData = {
     message: 'The page you are looking for was not found.',
@@ -147,7 +144,7 @@ const anythingElse = (request, response) => {
   respondWithFormat(request, response, 404, responseData);
 };
 
-//Export
+// Export
 module.exports = {
   getIndex,
   getCSS,
--- src/server.js
+++ src/server.js
@@ -1,43 +1,39 @@
 const http = require('http');
 const responseHandler = require('./responses.js');
 
-//Set port
+// Set port
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-//URLS
+// URLS
 const urlStruct = {
-    '/': responseHandler.getIndex,
-    '/success': responseHandler.success,
-    '/badRequest': responseHandler.badRequest,
-    '/unauthorized': responseHandler.unauthorized,
-    '/forbidden': responseHandler.forbidden,
-    '/internal': responseHandler.internal,
-    '/notImplemented': responseHandler.notImplemented,
-    '/style.css': responseHandler.getCSS,
+  '/': responseHandler.getIndex,
+  '/success': responseHandler.success,
+  '/badRequest': responseHandler.badRequest,
+  '/unauthorized': responseHandler.unauthorized,
+  '/forbidden': responseHandler.forbidden,
+  '/internal': responseHandler.internal,
+  '/notImplemented': responseHandler.notImplemented,
+  '/style.css': responseHandler.getCSS,
 };
-
 
 const onRequest = (request, response) => {
+  // Parse URL
+  const protocol = request.connection.encrypted ? 'https' : 'http';
+  const parsedUrl = new URL(request.url, `${protocol}://${request.headers.host}`);
+  const params = {
+    query: Object.fromEntries(parsedUrl.searchParams),
+  };
 
-    //Parse URL
-    const protocol = request.connection.encrypted ? 'https' : 'http';
-    const parsedUrl = new URL(request.url, `${protocol}://${request.headers.host}`);
-    const params = {
-        query: Object.fromEntries(parsedUrl.searchParams)
-    }
-
-    //Check if URL exists
-    if (urlStruct[parsedUrl.pathname]) {
-
-        //Call response handler (handles both JSON and XML internally)
-        urlStruct[parsedUrl.pathname](request, response, params);
-    } else {
-
-        //URL does not exist - call not found handler
-        responseHandler.anythingElse(request, response, params);
-    }
+  // Check if URL exists
+  if (urlStruct[parsedUrl.pathname]) {
+    // Call response handler (handles both JSON and XML internally)
+    urlStruct[parsedUrl.pathname](request, response, params);
+  } else {
+    // URL does not exist - call not found handler
+    responseHandler.anythingElse(request, response, params);
+  }
 };
 
-//Create server
+// Create server
 http.createServer(onRequest).listen(port);
 console.log(`Listening on 127.0.0.1: ${port}`);
